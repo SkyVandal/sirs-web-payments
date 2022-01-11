f4b2ffd96543114a4ea6a5d8adfc0f2401b4ea01
Alexlanca <alexandrelancach@gmail.com>
2022-01-11 17:14:23 +0000

    login & signin & signout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
-import React, { Component }  from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './views/auth/Login'; 
+import Signup from './views/auth/Signup';
+import Logout from './views/auth/Logout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/logout' element={<Logout/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
